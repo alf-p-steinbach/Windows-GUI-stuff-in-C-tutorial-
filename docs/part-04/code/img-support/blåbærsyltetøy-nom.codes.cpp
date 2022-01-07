cdd2c5d38ae967d36eb490299f34fd4418cdbd72
alf-p-steinbach <alf.p.steinbach@gmail.com>
2022-01-07 03:12:04 +0100

    -

---
+++ docs/part-04/code/img-support/blåbærsyltetøy-nom.codes.cpp
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+auto main() -> int
+{
+    using Byte = unsigned char;
+    for( const Byte v: "Blåbærsyltetøy, nom!" ) {
+        if( v ) { printf( "%d ", +v ); }
+    }
+    printf( "\n" );
+}
