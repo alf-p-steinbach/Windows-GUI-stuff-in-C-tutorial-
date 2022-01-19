ff0d24de541ef2411a6b1eea2004b6d28805cc6a
alf-p-steinbach <alf.p.steinbach@gmail.com>
2022-01-20 00:21:50 +0100

    -

--- docs/part-05/code/on-screen-graphics/v2/main.cpp
+++ docs/part-05/code/on-screen-graphics/v2/main.cpp
@@ -1,25 +1,31 @@
 ﻿# // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
 #include <wrapped-winapi/windows-h.hpp>
 
-auto main() -> int
+void draw_on( const HDC canvas, const RECT& area )
 {
     constexpr auto  orange      = COLORREF( RGB( 0xFF, 0x80, 0x20 ) );
     constexpr auto  yellow      = COLORREF( RGB( 0xFF, 0xFF, 0x20 ) );
     constexpr auto  blue        = COLORREF( RGB( 0, 0, 0xFF ) );
-    constexpr auto  no_window   = HWND( 0 );
-    constexpr auto  area        = RECT{ 10, 10, 10 + 400, 10 + 400 };
     
-    const HDC canvas = GetDC( no_window );
-    SelectObject( canvas, GetStockObject( DC_PEN ) );
-    SelectObject( canvas, GetStockObject( DC_BRUSH ) );
-
+    // Clear the background to blue.
     SetDCBrushColor( canvas, blue );
-    FillRect( canvas, &area, 0 );   // `0` works for me, but should perhaps be the DC brush.
+    FillRect( canvas, &area, 0 );
 
     // Draw a yellow circle filled with orange.
     SetDCPenColor( canvas, yellow );
     SetDCBrushColor( canvas, orange );
     Ellipse( canvas, area.left, area.top, area.right, area.bottom );
+}
+
+auto main() -> int
+{
+    constexpr auto  no_window   = HWND( 0 );
+
+    const HDC canvas = GetDC( no_window );
+    SelectObject( canvas, GetStockObject( DC_PEN ) );
+    SelectObject( canvas, GetStockObject( DC_BRUSH ) );
+    
+    draw_on( canvas, RECT{ 10, 10, 10 + 400, 10 + 400 } );
 
     ReleaseDC( no_window, canvas );
 }
