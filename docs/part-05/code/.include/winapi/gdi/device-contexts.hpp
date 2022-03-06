99d7005944c28f011e2c5b0b4065a47562918d25
alf-p-steinbach <alf.p.steinbach@gmail.com>
2022-03-06 12:04:45 +0100

    Update device-contexts.hpp

--- docs/part-05/code/.include/winapi/gdi/device-contexts.hpp
+++ docs/part-05/code/.include/winapi/gdi/device-contexts.hpp
@@ -5,12 +5,16 @@
 
 #include <stddef.h>         // size_t
 
+#include <tuple>            // std::(get, tie, tuple)
 #include <type_traits>      // std::is_same_v
 #include <utility>          // std::(index_sequence, 
 
 namespace winapi::gdi {
     namespace cu = cpp::util;
-    using cu::hopefully, cu::No_copying, cu::Types_;
+    using   cu::hopefully, cu::No_copying, cu::Types_;
+    using   std::get, std::tie, std::tuple,
+            std::index_sequence,
+            std::make_index_sequence;
 
     inline void make_practical( const HDC dc )
     {
@@ -24,29 +28,13 @@ namespace winapi::gdi {
     {
         HDC     m_handle;
 
-        // Expands a specified RECT argument into its four member values as arguments.
-        template<
-            size_t      rect_arg_index,
-            class       Api_func,
-            class...    Args,
-            size_t...   indices_before_rect,
-            size_t...   indices_after_rect       // 0-based, i.e. minus offset
-            >
-        void call_draw_with_rect_arg_expanded_(
-            const Api_func                              api_func,
-            const std::tuple<const Args&...>&           args_tuple,
-            std::index_sequence<indices_before_rect...> ,
-            std::index_sequence<indices_after_rect...>
-            ) const
-        {
-            const RECT& r = std::get<rect_arg_index>( args_tuple );
-            draw(
-                api_func,
-                std::get<indices_before_rect>( args_tuple )...,     // Can be empty, works.
-                r.left, r.top, r.right, r.bottom,
-                std::get< rect_arg_index + 1 + indices_after_rect >( args_tuple )...
-                );
-        }
+        template< size_t, class Api_func, class... Args, size_t... i_before, size_t... i_after>
+        inline void call_draw_with_rect_arg_expanded_(
+            const Api_func                          api_func,
+            const tuple<const Args&...>&            args_tuple,
+            index_sequence<i_before...>  ,
+            index_sequence<i_after...>
+            ) const;
 
     protected:
         inline virtual ~Dc() = 0;                           // Derived-class responsibility.
@@ -83,22 +71,7 @@ namespace winapi::gdi {
         }
 
         template< class Api_func, class... Args >
-        auto draw( const Api_func api_func, const Args&... args ) const
-            -> const Dc&
-        {
-            const int i_first_rect = Types_< Args... >::template index_of_first_< RECT >;
-            if constexpr( i_first_rect < 0 ) {
-                api_func( m_handle, args... );
-            } else {
-                call_draw_with_rect_arg_expanded_<i_first_rect>(
-                    api_func,
-                    std::tie( args... ),
-                    std::make_index_sequence<i_first_rect>(),
-                    std::make_index_sequence<sizeof...( Args ) - (i_first_rect + 1)>()
-                    );
-            }
-            return *this;
-        }
+        inline auto draw( const Api_func api_func, const Args&... args ) const -> const Dc&;
 
         auto handle() const -> HDC { return m_handle; }
         operator HDC() const { return handle(); }
@@ -108,6 +81,46 @@ namespace winapi::gdi {
 
     inline Dc::~Dc() {}
 
+    template<
+        size_t      rect_arg_index,
+        class       Api_func,
+        class...    Args,
+        size_t...   indices_before_rect,
+        size_t...   indices_after_rect       // 0-based, i.e. minus offset
+        >
+    inline void Dc::call_draw_with_rect_arg_expanded_(
+        const Api_func                          api_func,
+        const tuple<const Args&...>&            args_tuple,
+        index_sequence<indices_before_rect...>  ,
+        index_sequence<indices_after_rect...>
+        ) const
+    {
+        const RECT& r = get<rect_arg_index>( args_tuple );
+        draw(
+            api_func,
+            get<indices_before_rect>( args_tuple )...,     // Can be empty, works.
+            r.left, r.top, r.right, r.bottom,
+            get< rect_arg_index + 1 + indices_after_rect >( args_tuple )...
+            );
+    }
+
+    template< class Api_func, class... Args >
+    inline auto Dc::draw( const Api_func api_func, const Args&... args ) const
+        -> const Dc&
+    {
+        const int i_first_rect = Types_< Args... >::template index_of_first_< RECT >;
+        if constexpr( i_first_rect < 0 ) {
+            api_func( m_handle, args... );
+        } else {
+            call_draw_with_rect_arg_expanded_<i_first_rect>(
+                api_func,
+                tie( args... ),
+                make_index_sequence<i_first_rect>(),
+                make_index_sequence<sizeof...( Args ) - (i_first_rect + 1)>()
+                );
+        }
+        return *this;
+    }
 
     class Screen_dc: public Dc
     {
