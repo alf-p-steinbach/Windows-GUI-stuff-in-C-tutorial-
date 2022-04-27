fa618322dd455d414c3ec595d3675bc6814a3fe0
alf-p-steinbach <alf.p.steinbach@gmail.com>
2022-04-27 23:05:24 +0200

    -

--- README.md.files/hello-world/build.msvc.bat
+++ README.md.files/hello-world/build.msvc.bat
@@ -1,8 +1,11 @@
 @echo off & setlocal
-rc /nologo /c 65001 resources.rc
+md .build 2>nul
+pushd .build
+rc /nologo /c 65001 /Fo"resources.res" ..\resources.rc
 
 set CL=^
   /nologo /utf-8 /EHsc /GR /permissive- /std:c++17 /Zc:__cplusplus /Zc:externC- /W4 /wd4459 ^
   /D _CRT_SECURE_NO_WARNINGS=1 /D _STL_SECURE_NO_WARNINGS=1
 set LINK=/entry:mainCRTStartup
-cl main.cpp user32.lib resources.res /link /subsystem:windows /out:"hello.exe"
+cl ..\main.cpp user32.lib resources.res /link /subsystem:windows /out:"hello.exe"
+popd
