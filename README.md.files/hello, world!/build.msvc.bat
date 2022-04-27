193d83837ed21384604b03787ecdf8ec0e452f51
alf-p-steinbach <alf.p.steinbach@gmail.com>
2022-04-27 22:29:02 +0200

    -

---
+++ README.md.files/hello, world!/build.msvc.bat
@@ -0,0 +1,8 @@
+@echo off & setlocal
+rc /nologo /c 65001 resources.rc
+
+set CL=^
+  /nologo /utf-8 /EHsc /GR /permissive- /std:c++17 /Zc:__cplusplus /Zc:externC- /W4 /wd4459 ^
+  /D _CRT_SECURE_NO_WARNINGS=1 /D _STL_SECURE_NO_WARNINGS=1
+set LINK=/entry:mainCRTStartup
+cl main.cpp user32.lib resources.res /link /subsystem:windows /out:"hello.exe"
---
+++ README.md.files/hello, world!/main.cpp
@@ -0,0 +1,50 @@
+﻿#include <windows.h>
+#include <commctrl.h>           // InitCommonControlsEx, ICC_*
+#include "resource-ids.h"       // IDI_MAIN
+
+#ifdef _MSC_VER
+#   pragma comment( lib, "Comctl32" )
+#endif
+
+constexpr auto utf8_is_the_execution_character_set()
+    -> bool
+{
+    constexpr auto& slashed_o = "ø";
+    return (sizeof( slashed_o ) == 3 and slashed_o[0] == '\xC3' and slashed_o[1] == '\xB8');
+}
+
+static_assert(
+    utf8_is_the_execution_character_set(),
+    "The execution character set must be UTF-8 (e.g. MSVC option \"/utf-8\")."
+    );
+
+
+constexpr DWORD basic_common_controls = ICC_STANDARD_CLASSES | ICC_WIN95_CLASSES;
+
+auto init_common_controls( const DWORD which = basic_common_controls )
+    -> bool
+{
+    const INITCOMMONCONTROLSEX params = {sizeof( params ), which};
+    return !!InitCommonControlsEx( &params );
+}
+
+auto main()
+    -> int
+{
+    if( GetACP() != CP_UTF8 ) {
+        return EXIT_FAILURE;    // Not UTF-8 support for MessageBox.
+    }
+
+    init_common_controls();
+
+    MSGBOXPARAMS params = {sizeof( MSGBOXPARAMS )};
+    params.lpszCaption  = "Hello, world!";
+    params.lpszText     = "Did you know, that almost every 日本国 кошка likes Norwegian blåbærsyltetøy?\n"
+                          "\n"
+                          "(Click the OK button to quit, please)";
+    params.lpszIcon     = MAKEINTRESOURCE( IDI_MAIN );
+    params.hInstance    = GetModuleHandle( nullptr );
+    params.dwStyle      = MB_SETFOREGROUND | MB_USERICON | MB_TASKMODAL;    // Windows 11.
+    
+    MessageBoxIndirect( &params );
+}
